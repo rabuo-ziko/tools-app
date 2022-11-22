f990f3da6f36918697b1804a85e4847df3fb3b79
UserName <user.email@email.com>
2022-11-23 01:01:40 +0900

    デプロイ

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<tools-app>/',
+  base: '/tools-app/',
   build: {
     outDir: 'docs'
   },
